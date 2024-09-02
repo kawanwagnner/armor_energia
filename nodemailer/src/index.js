fd4ff821d89ac938f4ade07ca1f49af2629894d8
Kawan Wagnner <kawanwagnner7@gmail.com>
2024-09-02 16:22:35 -0300

    new changes

--- nodemailer/src/index.js
+++ nodemailer/src/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-const multer = require("multer");
 const path = require("path");
-const fs = require("fs");
 const SMTP_CONFIG = require("./config/smtp");
 
 const app = express();
@@ -12,14 +10,6 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
-
-// Certifica-se de que a pasta 'uploads' existe
-if (!fs.existsSync("uploads")) {
-  fs.mkdirSync("uploads");
-}
-
-// Configuração do multer para upload de arquivos
-const upload = multer({ dest: "uploads/" });
 
 // Configuração do transporte do nodemailer
 const transporter = nodemailer.createTransport({
@@ -41,37 +31,28 @@ app.post("/test", (req, res) => {
 });
 
 // Rota para enviar Email
-app.post("/send-email", upload.single("arquivo"), async (req, res) => {
-  const { nome, email, tel, assunto, msg, whatsapp } = req.body;
-  const arquivo = req.file;
+app.post("/send-email", async (req, res) => {
+  const { nome, email, zap, mensagem, economiaPorAno, economiaPorMes } =
+    req.body;
 
   try {
     const mailOptions = {
       from: `"${nome}" <${email}>`,
       to: "armorenergia.contato@gmail.com",
-      subject: assunto,
+      subject: zap,
       html: `
         <html>
         <body>
           <p><strong>Nome:</strong> ${nome}</p>
           <p><strong>Email:</strong> ${email}</p>
-          <p><strong>tel:</strong> ${tel}</p>
-          <p><strong>Assunto:</strong> ${assunto}</p>
-          <p><strong>msg:</strong> ${msg}</p>
-          <p><strong>WhatsApp:</strong> ${whatsapp}</p>
+          <p><strong>WhatsApp:</strong> ${zap}</p>
+          <p><strong>Mensagem:</strong> ${mensagem}</p>
+          <p><strong>Economia por Ano (Com desconto de 40%):</strong> R$ ${economiaPorAno}</p>
+          <p><strong>Economia por Mês (Com desconto de 40%):</strong> R$ ${economiaPorMes}</p>
         </body>
         </html>
       `,
     };
-
-    if (arquivo) {
-      mailOptions.attachments = [
-        {
-          filename: arquivo.originalname,
-          path: arquivo.path,
-        },
-      ];
-    }
 
     // Envia o email e captura as informações de envio
     const mailSent = await transporter.sendMail(mailOptions);
@@ -85,26 +66,12 @@ app.post("/send-email", upload.single("arquivo"), async (req, res) => {
       console.log("Nenhum destinatário aceitou o email.");
     }
 
-    if (arquivo) {
-      // Remove o arquivo temporário
-      fs.unlink(arquivo.path, (err) => {
-        if (err) console.error("Erro ao remover o arquivo temporário:", err);
-      });
-    }
-
     // Envia uma resposta ao cliente com informações do envio
     res
       .status(200)
       .send(`Email enviado com sucesso para: ${mailSent.accepted.join(", ")}`);
   } catch (error) {
     console.error("Erro ao enviar o Email:", error);
-
-    if (arquivo) {
-      fs.unlink(arquivo.path, (err) => {
-        if (err) console.error("Erro ao remover o arquivo temporário:", err);
-      });
-    }
-
     res.status(500).send(`Erro ao enviar o Email: ${error.message}`);
   }
 });
